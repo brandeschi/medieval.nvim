51c64dbdb2b615c83c4117d17181796ef1662898
brandeschi <contact.brandontedeschi@gmail.com>
2023-05-11 22:21:12 -0400

    update: color changes

--- lua/medieval/groups.lua
+++ lua/medieval/groups.lua
@@ -28,7 +28,7 @@ M.setup = function()
 
   local groups = {
     -- Base groups
-    -- TODO: Overwrite the base ones
+    -- TODO: Change Link names and finish filling
     GruvboxFg0 = { fg = palette.semiSilver },
     GruvboxFg1 = { fg = palette.traditionalTan }, -- Vars
     GruvboxFg2 = { fg = palette.none },
@@ -40,22 +40,22 @@ M.setup = function()
     GruvboxBg2 = { fg = palette.none },
     GruvboxBg3 = { fg = palette.none },
     GruvboxBg4 = { fg = palette.none },
-    GruvboxRed = { fg = palette.slightSilver }, -- Keywords (RN is purple color but maybe I should swap this with current 'constant' color [silverlike])
+    GruvboxRed = { fg = palette.slightlySilver }, -- Keywords
     GruvboxRedBold = { fg = palette.broBronze, bold = config.bold },
     GruvboxGreen = { fg = palette.none },
     GruvboxGreenBold = { fg = palette.gleefulGreen, bold = config.bold }, -- func
     GruvboxYellow = { fg = palette.boldRoyalBlue }, -- types
     GruvboxYellowBold = { fg = palette.willingWhite, bold = config.bold }, -- ModeMsg
-    GruvboxBlue = { fg = palette.babyBlue }, -- Prop
+    GruvboxBlue = { fg = palette.boringBlue }, -- Prop
     GruvboxBlueBold = { fg = palette.traditionalTan, bold = config.bold }, -- cmp matching
-    GruvboxPurple = { fg = palette.proudPink },
-    GruvboxPurpleBold = { fg = palette.proudPink, bold = config.bold }, -- number
+    GruvboxPurple = { fg = palette.giantGreen },
+    GruvboxPurpleBold = { fg = palette.giantGreen, bold = config.bold }, -- number
     GruvboxAqua = { fg = palette.prettyPurple }, -- This is the color I am talking about in GruvboxRed comment
     GruvboxAquaBold = { fg = palette.none, bold = config.bold },
     GruvboxOrange = { fg = palette.willingWhite }, -- Links to punctuation
     GruvboxOrangeBold = { fg = palette.boldRoyalBlue, bold = config.bold },
-    GruvboxRedSign = config.transparent_mode and { fg = palette.randomRed, reverse = config.invert_signs }
-      or { fg = palette.randomRed, bg = palette.basicallyBlack, reverse = config.invert_signs },
+    GruvboxRedSign = config.transparent_mode and { fg = palette.royaleRed, reverse = config.invert_signs }
+      or { fg = palette.royaleRed, bg = palette.basicallyBlack, reverse = config.invert_signs },
     GruvboxGreenSign = config.transparent_mode and { fg = palette.gleefulGreen, reverse = config.invert_signs }
       or { fg = palette.gleefulGreen, bg = palette.basicallyBlack, reverse = config.invert_signs },
     GruvboxYellowSign = config.transparent_mode and { fg = palette.none, reverse = config.invert_signs }
@@ -64,14 +64,14 @@ M.setup = function()
       or { fg = palette.none, bg = palette.none, reverse = config.invert_signs },
     GruvboxPurpleSign = config.transparent_mode and { fg = palette.none, reverse = config.invert_signs }
       or { fg = palette.none, bg = palette.none, reverse = config.invert_signs },
-    GruvboxAquaSign = config.transparent_mode and { fg = palette.slightSilver, reverse = config.invert_signs }
-      or { fg = palette.slightSilver, bg = palette.basicallyBlack, reverse = config.invert_signs },
+    GruvboxAquaSign = config.transparent_mode and { fg = palette.slightlySilver, reverse = config.invert_signs }
+      or { fg = palette.slightlySilver, bg = palette.basicallyBlack, reverse = config.invert_signs },
     GruvboxOrangeSign = config.transparent_mode and { fg = palette.none, reverse = config.invert_signs }
       or { fg = palette.none, bg = palette.none, reverse = config.invert_signs },
     GruvboxRedUnderline = { undercurl = config.undercurl, sp = palette.none },
     GruvboxGreenUnderline = { undercurl = config.undercurl, sp = palette.none },
     GruvboxYellowUnderline = { undercurl = config.undercurl, sp = palette.none },
-    GruvboxBlueUnderline = { undercurl = config.undercurl, sp = palette.proudPink },
+    GruvboxBlueUnderline = { undercurl = config.undercurl, sp = palette.giantGreen },
     GruvboxPurpleUnderline = { undercurl = config.undercurl, sp = palette.none },
     GruvboxAquaUnderline = { undercurl = config.undercurl, sp = palette.none },
     GruvboxOrangeUnderline = { undercurl = config.undercurl, sp = palette.none },
@@ -84,7 +84,7 @@ M.setup = function()
     TabLineFill = { fg = palette.willingWhite, bg = palette.silentSlate, reverse = config.invert_tabline },
     TabLineSel = { fg = palette.willingWhite, bg = palette.basicallyBlack, reverse = config.invert_tabline },
     TabLine = { link = "TabLineFill" },
-    MatchParen = { fg = palette.randomRed, bg = palette.basicallyBlack, bold = config.bold },
+    MatchParen = { fg = palette.royaleRed, bg = palette.basicallyBlack, bold = config.bold },
     ColorColumn = { bg = palette.none },
     Conceal = { fg = palette.none },
     CursorLineNr = { fg = palette.traditionalTan, bg = palette.basicallyBlack },
@@ -96,7 +96,7 @@ M.setup = function()
     IncSearch = { fg = palette.boldRoyalBlue, bg = palette.willingWhite, reverse = config.inverse },
     CurSearch = { link = "IncSearch" },
     QuickFixLine = { fg = palette.none, bg = palette.none, bold = config.bold },
-    Underlined = { fg = palette.proudPink, underline = config.underline },
+    Underlined = { fg = palette.giantGreen, underline = config.underline },
     StatusLine = { fg = palette.traditionalTan, bg = palette.silentSlate, reverse = config.inverse },
     StatusLineNC = { fg = palette.traditionalTan, bg = palette.basicallyBlack, reverse = config.inverse },
     WinBar = { link = "Normal" },
@@ -105,14 +105,14 @@ M.setup = function()
     WildMenu = { fg = palette.traditionalTan, bg = palette.silentSlate, bold = config.bold },
     Directory = { link = "GruvboxBlueBold" },
     Title = { link = "GruvboxGreenBold" },
-    ErrorMsg = { fg = palette.randomRed, bg = palette.basicallyBlack, bold = config.bold },
+    ErrorMsg = { fg = palette.royaleRed, bg = palette.basicallyBlack, bold = config.bold },
     MoreMsg = { link = "GruvboxYellowBold" },
     ModeMsg = { link = "GruvboxYellowBold" },
     Question = { link = "GruvboxOrangeBold" },
     WarningMsg = { link = "GruvboxRedBold" },
     LineNr = { fg = palette.shiningSilver }, -- @Link
     SignColumn = config.transparent_mode and { bg = nil } or { bg = palette.basicallyBlack }, --@Link
-    Folded = { fg = palette.proudPink, bg = palette.shiningSilver, italic = config.italic.folds },
+    Folded = { fg = palette.giantGreen, bg = palette.shiningSilver, italic = config.italic.folds },
     FoldColumn = config.transparent_mode and { fg = palette.traditionalTan, bg = nil } or { fg = palette.traditionalTan, bg = palette.willingWhite }, -- IDK
     Cursor = { reverse = config.inverse },
     vCursor = { link = "Cursor" },
@@ -129,7 +129,7 @@ M.setup = function()
     Repeat = { link = "GruvboxRed" },
     Label = { link = "GruvboxRed" },
     Exception = { link = "GruvboxRed" },
-    Operator = { fg = palette.randomRed, italic = config.italic.operators },
+    Operator = { fg = palette.royaleRed, italic = config.italic.operators },
     Keyword = { link = "GruvboxRed" },
     Identifier = { link = "GruvboxBlue" },
     Function = { link = "GruvboxGreenBold" },
@@ -154,7 +154,7 @@ M.setup = function()
     PmenuSel = { fg = palette.traditionalTan, bg = palette.shiningSilver, bold = config.bold },
     PmenuSbar = { bg = palette.silentSlate },
     PmenuThumb = { bg = palette.gleefulGreen },
-    DiffDelete = { fg = palette.randomRed, bg = palette.basicallyBlack, reverse = config.inverse },
+    DiffDelete = { fg = palette.royaleRed, bg = palette.basicallyBlack, reverse = config.inverse },
     DiffAdd = { fg = palette.gleefulGreen, bg = palette.basicallyBlack, reverse = config.inverse },
     DiffChange = { fg = palette.boldRoyalBlue, bg = palette.basicallyBlack, reverse = config.inverse },
     DiffText = { fg = palette.traditionalTan, bg = palette.basicallyBlack, reverse = config.inverse },
@@ -255,7 +255,7 @@ M.setup = function()
     ["@type.definition"] = { link = "Typedef" },
     ["@storageclass"] = { link = "StorageClass" },
     ["@attribute"] = { link = "PreProc" },
-    ["@field"] = { link = "Identifier" },
+    ["@field"] = { link = "GruvboxAqua" },
     ["@property"] = { link = "Identifier" },
     ["@variable"] = { link = "GruvboxFg1" },
     ["@variable.builtin"] = { link = "Special" },
@@ -466,7 +466,7 @@ M.setup = function()
     htmlArg = { link = "GruvboxOrange" },
     htmlTagN = { link = "GruvboxFg1" },
     htmlSpecialTagName = { link = "GruvboxBlue" },
-    htmlLink = { fg = palette.proudPink, underline = config.underline },
+    htmlLink = { fg = palette.giantGreen, underline = config.underline },
     htmlSpecialChar = { link = "GruvboxRed" },
     htmlBold = { fg = palette.none, bg = palette.none, bold = config.bold },
     htmlBoldUnderline = { fg = palette.none, bg = palette.none, bold = config.bold, underline = config.underline },
--- lua/medieval/palette.lua
+++ lua/medieval/palette.lua
@@ -1,28 +1,25 @@
 local M = {}
 
 M.palette = {
-    basicallyBlack = "#0B0C0D",
-    -- basicallyBlack = "#ABCFC8",
-    silentSlate =    "#17191B",
-    willingWhite =   "#C4C2BF",
-    traditionalTan = "#DCC091",
-    prettyPurple =   "#BAA3F9",
-    -- gleefulGreen =   "#90C080", 20% brighter
-    gleefulGreen =   "#7AB367",
-    boldRoyalBlue =  "#4169E1",
-    babyBlue =       "#5572AA",
-    -- babyBlue =       "#424F5C",
-    -- proudPink = 	 "#F0A0B4",
-    proudPink = 	 "#97C8A5",
-    goodGreen =      "#3ABA84",
-    randomRed =      "#E14169",
-    -- slightSilver =   "#7598BB",
-    slightSilver =   "#90ACC8", -- Needs to be more redish or white
-    semiSilver =     "#4F4E52",
-    shiningSilver =  "#3B3E41",
-    broBronze =      "#CD7F32",
+    basicallyBlack = "#0B0C0D", -- BG
+    silentSlate =    "#17191B", -- CursorLine color
+    willingWhite =   "#C4C2BF", -- () & {} & []
+    traditionalTan = "#DCC091", -- Normal text
+    prettyPurple =   "#BAA3F9", -- Macros & builtin stuff
+    boldRoyalBlue =  "#4169E1", -- Types/Special
+    boringBlue =     "#5572AA", -- Props/slightly less imp than brb
+    giantGreen = 	 "#87B494", -- Number/Data
+    gleefulGreen =   "#7AB367", -- Functions
+    goodGreen =      "#2E9469", -- Strings
+    royaleRed =      "#E14169", -- OP symbols
+    slightlySilver = "#6C747A", -- Keywords
+    semiSilver =     "#3F3E41", -- Comments
+    shiningSilver =  "#3B3E41", -- Brighter silentSlate
+    broBronze =      "#CD7F32", -- Class/Struct
 
-    none = "#FF0000",
+    -- goodGreen =   "#3ABA84",
+    -- proudPink = 	 "#F0A0B4",
+    none = "#FF00FF",
 }
 
 return M
