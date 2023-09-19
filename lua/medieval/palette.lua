95ffc446e706f06d45831410154e5010d3665b72
brandeschi <contact.brandontedeschi@gmail.com>
2023-09-18 22:27:49 -0400

    new colors test

--- lua/medieval/palette.lua
+++ lua/medieval/palette.lua
@@ -1,16 +1,22 @@
 local M = {}
 
 M.palette = {
-    basementBlue = "#1B1C1D", -- BG
+    -- basementBlue = "#1B1C1D", -- BG
+    -- basementBlue = "#20212B", -- BG
+    basementBlue = "#191A22", -- BG
     basicallyBlack = "#0B0C0D", -- CursorLine
     -- silentSlate =    "#17191B", -- CursorLine color
     willingWhite =   "#C4C2BF", -- () & {} & []
     traditionalTan = "#DCC091", -- Normal text
     prettyPurple =   "#BAA3F9", -- Macros & builtin stuff
     boldRoyalBlue =  "#4169E1", -- Types/Special
-    boringBlue =     "#5572AA", -- Props/slightly less imp than brb
-    giantGreen = 	 "#87B494", -- Number/Data
-    gleefulGreen =   "#7AB367", -- Functions
+    -- boringBlue =     "#5572AA", -- Props/slightly less imp than brb
+    boringBlue =     "#B3C3F3", -- Props/slightly less imp than brb
+    -- giantGreen = 	 "#87B494", -- Number/Data
+    giantGreen = 	 "#B9E4C5", -- Number/Data
+    -- gleefulGreen =   "#7AB367", -- Functions
+    -- gleefulGreen =   "#2DBA76", -- Functions
+    gleefulGreen =   "#5C968B", -- Functions
     royaleRed =      "#E14169", -- OP symbols
     slightlySilver = "#6C747A", -- Keywords
     semiSilver =     "#525154", -- Comments
