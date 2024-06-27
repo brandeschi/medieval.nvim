585a6cd9bf2e233bb2a1e593219e7732e9c70f68
brandeschi <contact.brandontedeschi@gmail.com>
2024-06-27 00:20:26 -0400

    cleanup gruvbox groups and fix side effects from it

--- lua/medieval/palette.lua
+++ lua/medieval/palette.lua
@@ -1,44 +1,25 @@
 local M = {}
 
 M.palette = {
-    -- basementBlue = "#1B1C1D", -- BG
-    basementBlue = "#20212B", -- BG
-    -- basementBlue = "#3B3E41", -- BG
-    -- basicallyBlack = "#0B0C0D", -- CursorLine
-    -- basicallyBlack = "#10141F", -- CursorLine
-    basicallyBlack = "#181818", -- CursorLine
-    -- basicallyBlack = "#EDDBBC", -- CursorLine
-    silentSlate =    "#17191B", -- CursorLine color
-    willingWhite =   "#8C8C8C", -- () & {} & []
-    -- willingWhite =   "#DBD9D6", -- () & {} & []
-    -- traditionalTan = "#DCC091", -- Normal text
-    traditionalTan = "#C6AC82", -- Normal text
-    -- prettyPurple =   "#BAA3F9", -- Macros & builtin stuff
-    prettyPurple =   "#9386B1", -- Macros & builtin stuff
-    -- prettyPurple =   "#9055DB", -- Macros & builtin stuff
-    -- boldRoyalBlue =  "#2D4FFF", -- Types/Special
-    -- boldRoyalBlue =  "#4169E1", -- Types/Special
-    -- boldRoyalBlue =  "#839DEB", -- Types/Special
-    boldRoyalBlue =  "#6585E7", -- Types/Special
-    -- boringBlue =     "#5572AA", -- Props/slightly less imp than brb
-    boringBlue =     "#D9D9D9", -- Props/slightly less imp than brb
-    -- boringBlue =     "#606677", -- Props/slightly less imp than brb
-    -- giantGreen = 	 "#87B494", -- Number/Data
-    -- giantGreen = 	 "#95B89F", -- Number/Data
-    giantGreen = 	 "#A6CDB1", -- Number/Data
-    -- gleefulGreen =   "#7AB367", -- Functions
-    -- gleefulGreen =   "#2DBA76", -- Functions
-    -- gleefulGreen =   "#5C968B", -- Functions
-    -- gleefulGreen =   "#3C8779", -- Functions
-    poshPink =       "#07C3B9",
-    gleefulGreen =   "#589C8E", -- Functions
-    royaleRed =      "#E14169", -- OP symbols
-    slightlySilver = "#6C747A", -- Keywords
-    semiSilver =     "#525154", -- Comments
-    shiningSilver =  "#3B3E41", -- Brighter silentSlate
-    broBronze =      "#CD7F32", -- Class/Struct
+  basementBlue =   "#20212B", -- CursorLine
+  basicallyBlack = "#181818", -- BG
+  boldRoyalBlue =  "#6585E7", -- Types/Special
+  -- boringBlue =     "#5572AA", -- Props/slightly less imp than brb
+  -- boringBlue =     "#D9D9D9", -- Props/slightly less imp than brb
+  boringBlue =     "#36648B", -- Props/slightly less imp than brb
+  broBronze =      "#CD7F32", -- Class/Struct
+  electricBlue =   "#07C3B9", -- Number/Data
+  giantGreen = 	   "#A6CDB1", -- Strings
+  -- gleefulGreen =   "#3C8779", -- Functions
+  gleefulGreen =   "#589C8E", -- Functions
+  prettyPurple =   "#9386B1", -- Macros & builtin stuff
+  royaleRed =      "#E14169", -- OP symbols
+  semiSilver =     "#525154", -- WinSeparator
+  slightlySilver = "#6C747A", -- Comments
+  traditionalTan = "#C6AC82", -- Normal text
+  willingWhite =   "#8C8C8C", -- () & {} & []
 
-    none =           "#FF00FF",
+  none =           "#FF00FF",
 }
 
 return M
